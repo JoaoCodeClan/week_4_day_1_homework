d947528fc152eb3d1efc314d11a8b91eab190ffc
Joao <johnynecas@hotmail.com>
2017-10-16 21:06:32 +0100

    created controller for game

--- controller.rb
+++ controller.rb
@@ -4,7 +4,7 @@ require_relative('models/rock_paper.rb')
 
 
 get '/game/:hand1/:hand2' do
-
-game = RockPaperGame.new(params[:hand1],params[:hand2])
- result = game.play_hand()
+ game = RockPaperGame.new(params[:hand1], params[:hand2])
+ result = game.play_hand
+ return result
 end
