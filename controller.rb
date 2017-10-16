484901d8d26b6ee621394a151291de10a848e375
Joao <johnynecas@hotmail.com>
2017-10-16 21:40:39 +0100

    added css and missing files

--- controller.rb
+++ controller.rb
@@ -4,7 +4,12 @@ require_relative('models/rock_paper.rb')
 
 
 get '/game/:hand1/:hand2' do
- game = RockPaperGame.new(params[:hand1], params[:hand2])
- @game_winner = game.play_hand()
-erb(:result)
+  game = RockPaperGame.new(params[:hand1], params[:hand2])
+  @game_winner = game.play_hand()
+  erb(:result)
+end
+
+
+get '/' do
+  erb(:homepage)
 end
