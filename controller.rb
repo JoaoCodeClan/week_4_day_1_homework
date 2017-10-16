8df000158e8ad13c641c05ee2fd379a2afab3da5
Joao <johnynecas@hotmail.com>
2017-10-16 21:14:19 +0100

    added result erb

--- controller.rb
+++ controller.rb
@@ -5,6 +5,6 @@ require_relative('models/rock_paper.rb')
 
 get '/game/:hand1/:hand2' do
  game = RockPaperGame.new(params[:hand1], params[:hand2])
- result = game.play_hand
- return result
+ @game_winner = game.play_hand()
+erb(:result)
 end
